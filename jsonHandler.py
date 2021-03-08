402909e258fae2a42f3b72f5cd0729d24e371c34
RaposaClonada <71944464+RaposaClonada@users.noreply.github.com>
2021-03-08 15:36:33 -0300

    Summary (required)

--- jsonHandler.py
+++ jsonHandler.py
@@ -95,6 +95,7 @@ class JsonCRUD:
         file = open(self.filename)
         dictionary = json.loads(file.read())
         file.close()
+        if column not in dictionary['columns']: raise KeyError
         if type(value) not in (dict, list, tuple, str, float, int, bool, type(None)): value = None
         dictionary['CRUD'][index][column] = value
         file = open(self.filename, 'w')
--- soundtrackCRUD.py
+++ soundtrackCRUD.py
@@ -1,6 +1,7 @@
 import importlib
 
 JsonCRUD = importlib.import_module("jsonHandler").JsonCRUD
+time = importlib.import_module("time")
 
 class soundtrackCRUD(JsonCRUD):
     def __init__(self, filename):
@@ -8,3 +9,16 @@ class soundtrackCRUD(JsonCRUD):
         self.newColumn('Name')
         self.newColumn('Date of Creation')
         self.newColumn('Tags')
+    
+    def newSoundtrack(self, name: str):
+        self.newItem(name, time.time(), [])
+    
+    def addTag(self, index: int, tag: str):
+        tags = self[index]['Tags']
+        tags.append(tag)
+        self.updateItem(index, 'Tags', tags)
+    
+    def removeTag(self, index: int, tag: str):
+        tags = self[index]['Tags']
+        tags.remove(tag)
+        self.updateItem(index, 'Tags', tags)
